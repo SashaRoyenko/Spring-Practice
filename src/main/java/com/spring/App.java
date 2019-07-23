2561a830fd83ab3cdaca40e9bc418bbc7d4f70d9
Oleksandr Roienko <Oleksandr_Roienko@epam.com>
2019-07-23 14:14:30 +0300

    Tasks 1.2-1.3

--- src/main/java/com/spring/App.java
+++ src/main/java/com/spring/App.java
@@ -1,17 +1,27 @@
 package com.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     private Client client;
-    private ConsoleEventLogger eventLogger = new ConsoleEventLogger();
+    private EventLogger eventLogger;
 
-    private void logEvent(String msg){
+    public App(Client client, EventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
+
+    private void logEvent(String msg) {
         String message = msg.replaceAll(client.getId(), client.getFullName());
         eventLogger.logEvent(message);
     }
 
     public static void main(String[] args) {
-        App app = new App();
-        app.client = new Client("1", "Oleksandr Roienko");
+        ApplicationContext ctx =
+                new ClassPathXmlApplicationContext("spring.xml");
+        App app = (App) ctx.getBean("app");
         app.logEvent("Event for user 1");
+        app.logEvent("Event for user 2");
     }
 }
--- src/main/java/com/spring/ConsoleEventLogger.java
+++ src/main/java/com/spring/ConsoleEventLogger.java
@@ -1,7 +1,7 @@
 package com.spring;
 
-public class ConsoleEventLogger {
+public class ConsoleEventLogger implements EventLogger{
     public void logEvent(String msg){
-
+        System.out.println(msg);
     }
 }
